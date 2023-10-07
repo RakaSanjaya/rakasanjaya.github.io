038359dfa1b7f920504b2564659b54d08b014e65
Raka Sanjaya <sanjayaraka722@gmail.com>
2023-10-07 13:06:34 +0700

    second

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["inter", "Arial"],
+        signika: ["Signika Negative", "Arial", "sans-serif"],
+      },
+      colors: {
+        blue: "#00BCE1",
+        white: "#fafafa",
+        whiteDeep:"#cbd5e1",
+        grey: "#111111",
+        greylight: "#1D1D1D",
+        greylighten: "#323232",
+        dark: "#060606",
+      },
+    },
+  },
+  plugins: [],
+};
