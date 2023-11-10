8ffe5a217396a0a1bcc8c2bbce4e3f82959a95d6
Raka Sanjaya <sanjayaraka722@gmail.com>
2023-11-10 15:49:19 +0700

    fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,12 @@ module.exports = {
       fontFamily: {
         inter: ["inter", "Arial"],
         signika: ["Signika Negative", "Arial", "sans-serif"],
+        gabarito: ["Gabarito", "Arial"],
       },
       colors: {
         blue: "#00BCE1",
         white: "#fafafa",
-        whiteDeep:"#cbd5e1",
+        whiteDeep: "#cbd5e1",
         grey: "#111111",
         greylight: "#1D1D1D",
         greylighten: "#323232",
